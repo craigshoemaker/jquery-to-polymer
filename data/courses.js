ceb582875a39d3833a5900c3dff5c85d6f61c58f
Craig Shoemaker <craig@craigshoemaker.net>
2017-04-21 15:34:25 -0700

    Upate sample data

---
+++ data/courses.js
@@ -0,0 +1,56 @@
+window.navData = [
+    { "title":"AngularJS", "fileName": "angularjs", "children":[
+        { "title":"Beginner", "fileName": "angularjs-beginner", "children":[
+            { "title":"AngularJS: Getting Started", "fileName": "angularjs-getting-started", "children":[] },
+            { "title":"Angular Fundamentals", "fileName": "angular-fundamentals", "children":[] },
+            { "title":"Angular the Big Picture", "fileName": "angular-the-big-picture", "children":[] }        
+        ] },
+        { "title":"Intermediate", "fileName": "angularjs-intermediate", "children":[
+            { "title":"AngularJS Services In-Depth", "fileName": "angularjs-services-in-depth", "children":[] },
+            { "title":"Angular Routing In-Depth", "fileName": "angularjs-unit-testing-in-depth", "children":[] },
+            { "title":"AngularJS Line of Business Applications", "fileName": "angularjs-line-of-business-applications", "children":[] },
+            { "title":"Angular Best Practices", "fileName": "angular-best-practice", "children":[] },
+            { "title":"Angular Patterns: Clean Code", "fileName": "angular-patterns-clean-code", "children":[] }
+        ] },
+        { "title":"Advanced", "fileName": "angularjs-advanced", "children":[
+            { "title":"AngularJS In-Depth", "fileName": "angular-in-depth", "children":[] },
+            { "title":"Building a SPA Framework Using AngularJS", "fileName": "building-a-spa-framework-using-angular", "children":[] },
+            { "title":"An AngularJS Playbook", "fileName": "an-angularjs-playbook", "children":[] },
+            { "title":"Angular 2: First Look", "fileName": "angular-2-first-look", "children":[] },
+        ] },
+    ] },
+    { "title":"HTML5", "fileName": "html5", "children":[
+        { "title":"Beginner", "fileName": "html5-beginner", "children":[
+            { "title":"HTML Fundamentals", "fileName": "html-fundamentals", "children":[] },
+            { "title":"HTML5 Fundamentals", "fileName": "html5-fundamentals", "children":[] },
+            { "title":"Semantic HTML", "fileName": "semantic-html", "children":[] }
+        ] },
+        { "title":"Intermediate", "fileName": "html5-intermediate", "children":[
+            { "title":"Practical HTML", "fileName": "practical-html", "children":[] },
+            { "title":"HTML5 Web Components Fundamentals", "fileName": "html5-web-components-fundamentals", "children":[] },            
+            { "title":"WebRTC Fundamentals", "fileName": "webrtc-fundamentals", "children":[] },            
+            { "title":"HTML5 Browser Caching", "fileName": "html5-browser-caching", "children":[] }
+        ] },
+        { "title":"Advanced", "fileName": "html5-advanced", "children":[
+            { "title":"HTML5 Advanced Topics", "fileName": "html5-advanced-topics", "children":[] },
+            { "title":"HTML5 Web Storage, IndexedDB and File System", "fileName": "html5-web-storage-indexeddb-file-system", "children":[] }
+        ] },
+    ] },
+    { "title":"JavaScript", "fileName": "javascript", "children":[
+        { "title":"Beginner", "fileName": "javascript-beginner", "children":[
+            { "title":"Quick Start to JavaScript: Vol 1", "fileName": "quick-start-to-javascript-vol-1", "children":[] },
+            { "title":"Quick Start to JavaScript: Vol 2", "fileName": "quick-start-to-javascript-vol-2", "children":[] },
+            { "title":"Quick Start to JavaScript: Vol 3", "fileName": "quick-start-to-javascript-vol-3", "children":[] }
+        ] },
+        { "title":"Intermediate", "fileName": "javascript-intermediate", "children":[
+            { "title":"JavaScript Objects and Prototypes", "fileName": "javascript-objects-and-prototypes", "children":[] },
+            { "title":"Practical Design Patterns in JavaScript", "fileName": "practical-design-patterns-in-javascript", "children":[] },
+            { "title":"JavaScript Design Patterns", "fileName": "javascript-design-patterns", "children":[] }
+        ] },
+        { "title":"Advanced", "fileName": "javascript-advanced", "children":[
+            { "title":"Advanced Techniques in JavaScript and jQuery", "fileName": "advanced-techniques-in-javascript-and-jquery", "children":[] },
+            { "title":"JavaScript Best Practices", "fileName": "javascript-best-practices", "children":[] },
+            { "title":"Rapid ES6 Training", "fileName": "rapid-es6-training", "children":[] }
+        ] },
+    ] },
+];
